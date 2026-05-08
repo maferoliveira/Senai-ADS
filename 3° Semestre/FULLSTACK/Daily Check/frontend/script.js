ac2347a5e921254dc2b0e1422dee2c3548af85f3
maferoliveira <natureshine.store@gmail.com>
2026-05-08 08:42:23 -0300

    teste

--- 3° Semestre/FULLSTACK/Daily Check/frontend/script.js
+++ 3° Semestre/FULLSTACK/Daily Check/frontend/script.js
@@ -1,55 +1,77 @@
 var url = "http://localhost:3000/tarefas";
 var key = "648c9e4ef64735fd4b4f0db1222f825f";
 
-
-carregarTarefas();
+window.onload = function () {
+  carregarTarefas();
+};
 
 function carregarTarefas() {
   fetch(url + "/listar")
-.then(function(res) {
+    .then(function (res) {
       return res.json();
     })
-.then(function(tarefas) {
+    .then(function (tarefas) {
+      console.log(tarefas);
       mostrarTarefas(tarefas);
+    })
+    .catch(function (erro) {
+      console.log("Erro ao carregar tarefas:", erro);
+      alert("Erro ao carregar tarefas");
     });
 }
 
 function mostrarTarefas(tarefas) {
   var container = document.getElementById("tarefas-container");
-  if (!container) return;
+
+  if (!container) {
+    return;
+  }
 
   container.innerHTML = "";
 
-  if (tarefas.length == 0) {
+  if (tarefas.length === 0) {
     container.innerHTML = "<p>Nenhuma tarefa cadastrada</p>";
     return;
   }
 
   for (var i = 0; i < tarefas.length; i++) {
     var t = tarefas[i];
+
     var card = document.createElement("div");
     card.className = "card";
-    card.onclick = (function(id) {
-      return function() {
+
+    card.onclick = (function (id) {
+      return function () {
         editar(id);
-      }
+      };
     })(t.id);
 
-    
-    var linkImg = localStorage.getItem("img_" + t.id);
-
-   
     var imagemHtml = "";
-    if (linkImg && linkImg!= "") {
-      imagemHtml = '<img src="' + linkImg + '">';
+
+    if (t.img && t.img !== "") {
+      imagemHtml =
+        '<img src="' +
+        t.img +
+        '" style="width:100%; border-radius:10px;">';
     }
 
-    card.innerHTML = imagemHtml +
-      '<h3>' + t.nome + '</h3>' +
-      '<p>Início: ' + t.diainicio + '</p>' +
-      '<p>Fim: ' + t.diafim + '</p>' +
-      '<p>' + t.descricao + '</p>' +
-      '<button onclick="excluir(event, ' + t.id + ')">Excluir</button>';
+    card.innerHTML =
+      imagemHtml +
+      "<h3>" +
+      t.Nome +
+      "</h3>" +
+      "<p><b>Início:</b> " +
+      t.inicio +
+      "</p>" +
+      "<p><b>Fim:</b> " +
+      t.fim +
+      "</p>" +
+      "<p>" +
+      t.descrição +
+      "</p>" +
+      '<button onclick="excluir(event, ' +
+      t.id +
+      ')">Excluir</button>';
 
     container.appendChild(card);
   }
@@ -57,54 +79,76 @@ function mostrarTarefas(tarefas) {
 
 function excluir(event, id) {
   event.stopPropagation();
-  if (confirm("Deseja excluir tarefa?")) {
-    localStorage.removeItem("img_" + id);
-    fetch(url + "/excluir/" + id, { method: "DELETE" })
-.then(function() {
+
+  var confirmar = confirm("Deseja excluir tarefa?");
+
+  if (confirmar) {
+    fetch(url + "/excluir/" + id, {
+      method: "DELETE",
+    })
+      .then(function () {
         carregarTarefas();
+      })
+      .catch(function (erro) {
+        console.log("Erro ao excluir:", erro);
       });
   }
 }
 
 function editar(id) {
   fetch(url + "/buscar/" + id)
-.then(function(res) {
+    .then(function (res) {
       return res.json();
     })
-.then(function(t) {
-      document.getElementById("tituloModal").innerHTML = "Editar Tarefa";
-      document.getElementById("idTarefa").value = t.id;
-      document.getElementById("nome").value = t.nome;
-      document.getElementById("inicio").value = t.diainicio;
-      document.getElementById("fim").value = t.diafim;
-      document.getElementById("descricao").value = t.descricao;
-      document.getElementById("imagem").value = t.img;
+    .then(function (t) {
+      document.getElementById("tituloModal").innerHTML =
+        "Editar Tarefa";
 
-      var img = localStorage.getItem("img_" + t.id);
-      if (img) {
-        document.getElementById("imagem").value = img;
+      document.getElementById("idTarefa").value = t.id;
+      document.getElementById("nome").value = t.Nome;
+      document.getElementById("inicio").value = t.inicio;
+      document.getElementById("fim").value = t.fim;
+      document.getElementById("descricao").value =
+        t.descrição;
+
+      if (t.img) {
+        document.getElementById("imagem").value = t.img;
       } else {
         document.getElementById("imagem").value = "";
       }
 
-      document.getElementById("modal").style.display = "block";
+      document.getElementById("modal").style.display =
+        "block";
+    })
+    .catch(function (erro) {
+      console.log("Erro ao buscar tarefa:", erro);
     });
 }
 
 function abrirModalCriar() {
-  document.getElementById("tituloModal").innerHTML = "Nova Tarefa";
-  document.getElementById("idTarefa").value = "";
-  document.getElementById("modal").style.display = "block";
-}
+  document.getElementById("tituloModal").innerHTML =
+    "Nova Tarefa";
 
-function fecharModal() {
-  document.getElementById("modal").style.display = "none";
+  document.getElementById("idTarefa").value = "";
   document.getElementById("nome").value = "";
   document.getElementById("inicio").value = "";
   document.getElementById("fim").value = "";
   document.getElementById("descricao").value = "";
   document.getElementById("imagem").value = "";
+
+  document.getElementById("modal").style.display =
+    "block";
+}
+
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+
   document.getElementById("idTarefa").value = "";
+  document.getElementById("nome").value = "";
+  document.getElementById("inicio").value = "";
+  document.getElementById("fim").value = "";
+  document.getElementById("descricao").value = "";
+  document.getElementById("imagem").value = "";
 }
 
 function salvarTarefa() {
@@ -112,94 +156,137 @@ function salvarTarefa() {
   var nome = document.getElementById("nome").value;
   var inicio = document.getElementById("inicio").value;
   var fim = document.getElementById("fim").value;
-  var descricao = document.getElementById("descricao").value;
+  var descricao =
+    document.getElementById("descricao").value;
   var imagem = document.getElementById("imagem").value;
 
-  if (nome == "" || inicio == "" || fim == "" || descricao == "") {
+  if (
+    nome === "" ||
+    inicio === "" ||
+    fim === "" ||
+    descricao === ""
+  ) {
     alert("Preencha todos os campos!");
     return;
   }
 
   var dados = {
-    nome: nome,
-    diainicio: inicio,
-    diafim: fim,
-    descricao: descricao,
-    img: imagem
+    Nome: nome,
+    inicio: inicio,
+    fim: fim,
+    descrição: descricao,
+    img: imagem,
   };
 
-  if (id!= "") {
-   
+  if (id !== "") {
     fetch(url + "/atualizar/" + id, {
       method: "PUT",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(dados)
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
     })
- .then(function(res) {
-      return res.json();
-    })
- .then(function(data) {
-      if (imagem!= "") {
-        localStorage.setItem("img_" + id, imagem);
-      } else {
-        localStorage.removeItem("img_" + id);
-      }
-      fecharModal();
-      carregarTarefas();
-    });
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function () {
+        fecharModal();
+        carregarTarefas();
+      })
+      .catch(function (erro) {
+        console.log("Erro ao atualizar:", erro);
+      });
   } else {
-
     fetch(url + "/cadastrar", {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(dados)
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
     })
- .then(function(res) {
-      return res.json();
-    })
- .then(function(data) {
-      if (imagem!= "") {
-        localStorage.setItem("img_" + data.id, imagem);
-      }
-      fecharModal();
-      carregarTarefas();
-    });
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function () {
+        fecharModal();
+        carregarTarefas();
+      })
+      .catch(function (erro) {
+        console.log("Erro ao cadastrar:", erro);
+      });
   }
 }
 
-
 function buscarClima() {
   var cidade = document.getElementById("cidade").value;
-  if (cidade == "") {
+
+  if (cidade === "") {
+    alert("Digite uma cidade");
     return;
   }
 
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + key + "&lang=pt_br&units=metric")
-.then(function(res) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cidade +
+    "&appid=" +
+    key +
+    "&lang=pt_br&units=metric"
+  )
+    .then(function (res) {
       return res.json();
     })
-.then(function(dados) {
+    .then(function (dados) {
       if (dados.cod == "404") {
+        alert("Cidade não encontrada");
         return;
       }
+
       mostrarClima(dados);
+    })
+    .catch(function (erro) {
+      console.log("Erro ao buscar clima:", erro);
     });
 }
 
 function mostrarClima(dados) {
-  var container = document.getElementById("clima-container");
+  var container =
+    document.getElementById("clima-container");
+
+  if (!container) {
+    return;
+  }
+
+  container.innerHTML = "";
+
   var card = document.createElement("div");
+
   card.className = "card";
 
-  card.innerHTML = '<h3>' + dados.name + '</h3>' +
-    '<p style="font-size: 30px;">' + Math.floor(dados.main.temp) + '°C</p>' +
-    '<img src="https://openweathermap.org/img/wn/' + dados.weather[0].icon + '.png" style="width:50px; height:50px;">' +
-    '<p>' + dados.weather[0].description + '</p>' +
-    '<p>Umidade: ' + dados.main.humidity + '%</p>';
+  card.innerHTML =
+    "<h3>" +
+    dados.name +
+    "</h3>" +
+    '<p style="font-size:30px;">' +
+    Math.floor(dados.main.temp) +
+    "°C</p>" +
+    '<img src="https://openweathermap.org/img/wn/' +
+    dados.weather[0].icon +
+    '.png">' +
+    "<p>" +
+    dados.weather[0].description +
+    "</p>" +
+    "<p>Umidade: " +
+    dados.main.humidity +
+    "%</p>";
 
   container.appendChild(card);
 }
 
 function limparClima() {
-  document.getElementById("clima-container").innerHTML = "";
+  var container =
+    document.getElementById("clima-container");
+
+  if (container) {
+    container.innerHTML = "";
+  }
 }
